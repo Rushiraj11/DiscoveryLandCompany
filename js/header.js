26489b3ff3161fb79646e8e8eee8ffabcda6cad4
Rushiraj11 <grushiraj95@gmail.com>
2021-11-27 19:50:45 +0530

    Added: navigation for all pages in project

--- js/header.js
+++ js/header.js
@@ -1,16 +1,16 @@
 document.querySelector('#header').innerHTML = `
 <div class="header">
-<div class="logo">
+<div class="logo" onclick="window.location ='../Pages/homepage.html'">
     <div class="logo-name">
         <span class="logo-text1">Discovery </span>
         <span class="logo-text2">land company</span> 
     </div>
 </div>
     <div class="options">
-        <div>About DLC</div>
-        <div>Our Worlds</div>
-        <div>Experiences</div>
-        <div>Gallery</div>
-        <div>Press</div>  
+        <div onclick="window.location ='../Pages/AboutDLC.html'">About DLC</div>
+        <div onclick="window.location ='../Pages/Our Worlds.html'">Our Worlds</div>
+        <div onclick="window.location ='../Pages/Experiences.html'">Experiences</div>
+        <div onclick="window.location ='../Pages/Gallery.html'">Gallery</div>
+        <div onclick="window.location ='../Pages/Press.html'">Press</div>  
 </div>
 </div>`;
